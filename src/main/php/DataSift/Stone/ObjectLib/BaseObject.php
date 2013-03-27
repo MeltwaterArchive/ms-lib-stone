cae92329c8ca73af2a0ed7643957f08cdb304a4c
Stuart Herbert <stuart@stuartherbert.com>
2013-03-27 11:08:33 +0000

    Fixed: vastly improved the object merge algorithm for BaseObject

--- src/main/php/DataSift/Stone/ObjectLib/BaseObject.php
+++ src/main/php/DataSift/Stone/ObjectLib/BaseObject.php
@@ -34,7 +34,7 @@ use stdClass;
  * a lot less code in our particular JSON-driven environment :)
  *
  * @category Libraries
- * @package  Stone
+ * @package  Stone/ObjectLib
  * @author   Stuart Herbert <stuart.herbert@datasift.com>
  * @license  http://mediasift.com/licenses/internal MediaSift Internal License
  * @link     http://www.mediasift.com
@@ -42,62 +42,188 @@ use stdClass;
 
 class BaseObject extends stdClass
 {
+    // ====================================================================
+    //
+    // merging two objects together
+    //
+    // --------------------------------------------------------------------
+
     /**
      * Copy all of the properties from another object (loaded from a
      * .json file or received over the net) into our own properties
      *
-     * By default, we convert any objects nested inside the other object
-     * into JsonObjects too, so that all the helpers defined in BaseObject
+     * We convert any objects nested inside the other object into
+     * BaseObjects too, so that all the helpers defined in BaseObject
      * are available.
      *
-     * @param  mixed $json the object to copy
+     * @param  mixed $src the object to copy
      * @return void
      */
-    public function mergeFromObject($src, $deep = true)
+    public function mergeFrom($src)
     {
         // special case - do we have a basic value?
         if (!is_object($src) && !is_array($src))
         {
+            // as we don't know what else to call it, we'll just add it
+            // to 'value'
             $this->value = $src;
+
+            // nothing more to do here
             return;
         }
 
-        // take advantage of PHP's ability to iterate over an object
-        // and its properties
-        foreach ($src as $key => $value)
-        {
-            if ($deep && is_object($value) && !($value instanceof JsonObject))
-            {
-                $this->$key = $this->convertObject($value);
-            }
-            else if ($deep && is_array($value))
-            {
-                $this->$key = $this->convertArray($value);
-            }
-            else
-            {
-                $this->$key = $value;
-            }
-        }
+        // if we get here, then we have a complex structure to evaluate,
+        // which we're going to handle recursively
+        $this->mergeInto($this, $src);
     }
 
-    private function convertObject($src)
+    /**
+     * merge a (possibly complex) object or array into our (possibly complex)
+     * object or array
+     *
+     * along the way, we'll convert any plain old stdClass objects into
+     * funky new BaseObjects
+     *
+     * this was originally created for supporting merging two or more
+     * config files (e.g. a default one, and an overrides file) into
+     * one usable data structure
+     *
+     * PLEASE NOTE that this method is recursive, which will cause
+     * problems if you go nuts on how deep your data structures are
+     *
+     * @param  object|array $ours
+     *         the array or object that we're adding to
+     * @param  object|array $theirs
+     *         the array or object that we're merging from
+     * @return object|array
+     *         $ours, after we've merged across from $theirs
+     */
+    private function mergeInto($ours, $theirs)
+    {
+        // let's see what is inside their object
+        foreach ($theirs as $key => $value)
+        {
+            // special case - conversion only
+            if (!isset($ours->$key))
+            {
+                // what are we looking at inside their object?
+                if (is_object($value) && ($value instanceof stdClass))
+                {
+                    // for convenience, turn stdClass into more BaseObjects
+                    $ours->$key = $this->convertObject($value);
+                }
+                else if (is_array($value))
+                {
+                    // for convenience, turn any stdClass objects that are
+                    // inside this array into more BaseObjects
+                    $ours->$key = $this->convertArray($value);
+                }
+                else
+                {
+                    $ours->$key = $value;
+                }
+
+                // all done converting
+                continue;
+            }
+
+            // we have a clash ...
+            //
+            // if ours and theirs are incompatible data types, we will
+            // have to just overwrite
+            //
+            // this is how we handle merging into an existing object
+            if (is_object($ours->$key))
+            {
+                if (!is_object($value))
+                {
+                    // cannot merge object and non-object
+                    $ours->$key = $value;
+                    continue;
+                }
+
+                if (!$ours->$key instanceof stdClass || !$value instanceof stdClass)
+                {
+                    // we cannot merge any complex classes at all
+                    $ours->$key = $value;
+                    continue;
+                }
+
+                // if we get here, then we have two compatible objects to merge
+                //
+                // we don't know what's inside their object ... recursion to
+                // the rescue :(
+                $ours->$key = $this->mergeInto($ours->$key, $value);
+                continue;
+            }
+
+            // this is how we handle merging into an existing array
+            if (is_array($ours->$key))
+            {
+                if(!is_array($value))
+                {
+                    // cannot merge array and non-array
+                    $ours->$key = $value;
+                    continue;
+                }
+
+                // if we get here, then we have two arrays to merge
+                $ours->$key = $this->mergeInfo($ours->$key, $value);
+                continue;
+            }
+
+            // if we get here, then we have nothing that we can merge,
+            // and can only overwrite
+            $ours->$key = $value;
+        }
+
+        // all done
+        return $ours;
+    }
+
+    /**
+     * convert a stdClass into a BaseObject
+     *
+     * this does a deep conversion, and therefore can be recursive
+     *
+     * @param  stdClass $src
+     *         the object that we want to convert
+     * @return BaseObject
+     *         the replacement object
+     */
+    private function convertObject(stdClass $src)
     {
         $return = new BaseObject();
-        $return->mergeFromObject($src);
+        $return->mergeFrom($src);
 
         return $return;
     }
 
+    /**
+     * look inside an array, to see if there are any stdClass objects
+     * that need converting into BaseObjects
+     *
+     * this does a deep conversion, and therefore can be recursive
+     *
+     * @param  array $src
+     *         the array to convert
+     * @return array
+     *         the replacement array
+     */
     private function convertArray($src)
     {
         $mustConvert = false;
 
-        // special case - do we actually need to do any conversion?
+        // do we actually need to do any conversion?
+        // let's inspect the array to make a decision
         $keys = array_keys($src);
         foreach ($keys as $key)
         {
-            if (is_object($src[$key]) || is_array($src[$key]))
+            // we only want to convert in two cases
+            //
+            // we find a stdClass object (must be converted)
+            // we find a nested array (we must look deeper)
+            if ((is_object($src[$key]) && $src[$key] instanceof stdClass) || is_array($src[$key]))
             {
                 $mustConvert = true;
                 break;
@@ -111,25 +237,48 @@ class BaseObject extends stdClass
             return $src;
         }
 
+        // if we get here, then we need to convert what we have
+        //
+        // this is what we'll send back to the caller
         $return = array();
+
+        // let's iterate over the array once more
         foreach ($src as $key => $value)
         {
-            if (is_object($value))
+            // do we have a convertable object?
+            if (is_object($value) && $object instanceof stdClass)
             {
+                // yes - convert it
                 $return[$key] = $this->convertObject($value);
             }
+            // do we have a nested array?
             else if (is_array($value))
             {
+                // yes - let's look inside it, in case there are stdClass
+                // objects lurking within
                 $return[$key] = $this->convertArray($value);
             }
             else
             {
+                // whatever we have, it is not convertable
+                // just copy it across
                 $return[$key] = $value;
             }
         }
 
+        // all done - return the replacement array to the caller
         return $return;
     }
+
+    // ====================================================================
+    //
+    // Helpers
+    //
+    // These are mostly syntactic sugar, but they help a tiny bit with
+    // code robustness because they can deal correctly with unset properties,
+    // something many developers forget to check for
+    //
+    // --------------------------------------------------------------------
 
     /**
      * Do we have a named property set to non-null?
