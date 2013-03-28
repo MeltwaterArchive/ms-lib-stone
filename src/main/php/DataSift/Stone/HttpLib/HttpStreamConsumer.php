8170768758a13a774cc994cadb28194b6f5e86eb
Stuart Herbert <stuart@stuartherbert.com>
2013-03-28 15:29:52 +0000

    New: added a maxDuration parameter to HttpStreamConsumer, plus lots and lots of extra logging

--- src/main/php/DataSift/Stone/HttpLib/HttpStreamConsumer.php
+++ src/main/php/DataSift/Stone/HttpLib/HttpStreamConsumer.php
@@ -25,6 +25,7 @@ use DataSift\Stone\HttpLib\HttpClient;
 use DataSift\Stone\HttpLib\HttpClientRequest;
 use DataSift\Stone\HttpLib\HttpClientResponse;
 use DataSift\Stone\LogLib\Log;
+use DataSift\Stone\TimeLib\DateInterval;
 
 /**
  * A generic class for receiving data via HTTP streaming
@@ -44,9 +45,10 @@ class HttpStreamConsumer
      * @param array    $requests where to find the stream
      * @param callback $callback we call this after every piece of data is read from the stream
      */
-    public function consume($requests, $callback)
+    public function consume($requests, $callback, $duration = null)
     {
-        $streams = array();
+        // keep track of the number of active requests
+        $activeRequests = array();
 
         // deal with things when a non-array is passed in
         if (!is_array($requests))
@@ -54,6 +56,29 @@ class HttpStreamConsumer
             $requests = array($requests);
         }
 
+        // work out our starting time
+        $now = time();
+
+        // convert the max duration into seconds
+        if ($duration)
+        {
+            // how long are we consuming for?
+            $noOfRequests = count($requests);
+            Log::write(Log::LOG_INFO, "consuming {$noOfRequests} stream(s) for {$duration}");
+
+            // convert the duration into a time to stop
+            $interval = new DateInterval($duration);
+            $seconds  = $interval->getTotalSeconds();
+
+            $stopAt   = $now + $seconds;
+        }
+        else
+        {
+            // effectively, run forever
+            $stopAt = PHP_INT_MAX;
+        }
+
+        // make the initial requests for data
         foreach ($requests as $index => $request)
         {
             // lets get our requests made
@@ -69,36 +94,39 @@ class HttpStreamConsumer
             }
             else
             {
-                $streams[$index] = array('request' => $request, 'client' => $client, 'response' => $response);
+                $activeRequests[$index] = array('request' => $request, 'client' => $client, 'response' => $response);
 
                 // now, we need to make sure that we call the callback here
                 if (!call_user_func_array($callback, array($client, $request, $response)))
                 {
-                    // remove us from the list of streams
+                    // remove us from the list of active requests
                     $client->disconnect();
-                    unset($streams[$index]);
+                    unset($activeRequests[$index]);
                 }
             }
         }
 
-        // do we have any streams left?
-        if (count($streams) == 0)
+        // do we have any active requests left?
+        if (count($activeRequests) == 0)
         {
             // no ... job done
             return;
         }
 
-        // if we get here, then we have some streams to consume data from
+        // if we get here, then we have some active requests to consume data from
         $done = false;
         do
         {
+            // the time we started this loop
+            $now = time();
+
             // wait for a stream
             $readArray = array();
-            foreach ($streams as $streamIndex => $stream)
+            foreach ($activeRequests as $requestIndex => $request)
             {
-                if ($stream['client']->isConnected())
+                if ($request['client']->isConnected())
                 {
-                    $readArray[$streamIndex] = $stream['client']->getSocket();
+                    $readArray[$requestIndex] = $request['client']->getSocket();
                 }
             }
 
@@ -109,28 +137,54 @@ class HttpStreamConsumer
                 return;
             }
 
-            $sockets = $readArray;
+            $sockets    = $readArray;
             $writeArray = $exceptArray = array();
-            $activeStreams = array();
+
+            $noOfRequests = count($activeRequests);
+            Log::write(Log::LOG_DEBUG, "Waiting for data from {$noOfRequests} connection(s)");
 
             $selectedStreams = 0;
-            while ($selectedStreams == 0)
+            while ($selectedStreams == 0 && count($readArray) > 0 && $now < $stopAt)
             {
                 $readArray = $sockets;
 
                 // we use the scream operator here to suppress a stupid
                 // warning about the system call being interrupted by
                 // signals such as SIGALRM ... grrr at the warning!
-                $selectedStreams = @stream_select($readArray, $writeArray, $exceptArray, 30);
+                $selectedStreams = @stream_select($readArray, $writeArray, $exceptArray, 1);
+
+                // make sure we have the right time after our select()
+                $now = time();
+
+                $remainingDuration = $stopAt - $now;
+                Log::write(Log::LOG_DEBUG, "selectedStreams is {$selectedStreams}; remaining duration is {$remainingDuration}");
             }
 
+            // have we run out of time?
+            if ($now >= $stopAt)
+            {
+                // yes, we have
+                Log::write(Log::LOG_INFO, "Closing connection; duration of {$seconds} second(s) reached");
+
+                foreach ($activeRequests as $request)
+                {
+                    $request['client']->disconnect();
+                }
+                // all done
+                return;
+            }
+
+            // if we get here, then we have at least one connection
+            // to read data from
+            $noOfSockets = count($readArray);
+            Log::write(Log::LOG_DEBUG, "Data available from {$noOfSockets} connections");
             foreach ($readArray as $socket)
             {
-                $streamIndex = array_search($socket, $sockets);
+                $requestIndex = array_search($socket, $sockets);
 
-                $client   = $streams[$streamIndex]['client'];
-                $request  = $streams[$streamIndex]['request'];
-                $response = $streams[$streamIndex]['response'];
+                $client   = $activeRequests[$requestIndex]['client'];
+                $request  = $activeRequests[$requestIndex]['request'];
+                $response = $activeRequests[$requestIndex]['response'];
 
                 // step 1:
                 //
@@ -146,7 +200,7 @@ class HttpStreamConsumer
                     Log::write(Log::LOG_INFO, "Closing connection at the internal callback's request");
 
                     $client->disconnect();
-                    unset($streams[$streamIndex]);
+                    unset($activeRequests[$requestIndex]);
                 }
 
                 // step 3: was there a problem with the response?
@@ -159,17 +213,17 @@ class HttpStreamConsumer
 
                     Log::write(Log::LOG_INFO, "Closing connection after errors");
                     $client->disconnect();
-                    unset($streams[$streamIndex]);
+                    unset($activeRequests[$requestIndex]);
                 }
 
                 else if ($response->connectionMustClose())
                 {
                     Log::write(Log::LOG_INFO, "Closing connection (probably closed by remote end");
                     $client->disconnect();
-                    unset($streams[$streamIndex]);
+                    unset($activeRequests[$requestIndex]);
                 }
             }
         }
-        while (count($streams) > 0);
+        while (count($activeRequests) > 0);
     }
 }
